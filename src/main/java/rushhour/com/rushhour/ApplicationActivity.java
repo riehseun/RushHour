ea9a80f9b8c532c1869c1e0385ec246e2d1e07fd
Seungmoon <seungmoon.rieh@mail.utoronto.ca>
2016-04-02 10:36:45 -0400

    Initial project setup

---
+++ src/main/java/rushhour/com/rushhour/ApplicationActivity.java
@@ -0,0 +1,13 @@
+package rushhour.com.rushhour;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ApplicationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.applicationactivity);
+    }
+}
